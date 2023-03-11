aba94990b68107edcd1f64ad8b64cbc281778aa4
Mohamed <mohamed0909samy@gmail.com>
2023-03-12 01:22:18 +0200

    Updates

--- app/src/main/java/com/example/composenewsapp/presentation/news/NewsViewModel.kt
+++ app/src/main/java/com/example/composenewsapp/presentation/news/NewsViewModel.kt
@@ -2,9 +2,7 @@ package com.example.composenewsapp.presentation.news
 
 import androidx.lifecycle.viewModelScope
 import com.example.composenewsapp.domain.model.Article
-import com.example.composenewsapp.domain.model.BreakingNewsQuery
 import com.example.composenewsapp.domain.model.NewsQuery
-import com.example.composenewsapp.domain.use_cases.BreakingNewsUseCase
 import com.example.composenewsapp.domain.use_cases.NewsUseCase
 import com.example.composenewsapp.presentation.base.BaseState
 import com.example.composenewsapp.presentation.base.BaseViewModel
@@ -14,14 +12,12 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NewsViewModel @Inject constructor(
     private val newsUseCase: NewsUseCase,
-    private val breakingNewsUseCase: BreakingNewsUseCase
 ) : BaseViewModel(BaseState.Empty) {
 
     private val _newsState = MutableStateFlow(NewsState(state = BaseState.Empty))
@@ -29,9 +25,6 @@ class NewsViewModel @Inject constructor(
 
     private val _news = MutableStateFlow(listOf<Article>())
     val news: StateFlow<List<Article>> get() = _news
-
-    private val _breakingNews = MutableStateFlow(listOf<Article>())
-    val breakingNews: StateFlow<List<Article>> get() = _news
 
     fun requestNews(newsQuery: NewsQuery) = viewModelScope.launch(Dispatchers.IO) {
          newsUseCase(newsQuery).collect {
@@ -43,13 +36,4 @@ class NewsViewModel @Inject constructor(
          }
     }
 
-    fun requestBreakingNews(breakingNewsQuery: BreakingNewsQuery) = viewModelScope.launch(Dispatchers.IO) {
-        breakingNewsUseCase(breakingNewsQuery).collect {
-            when(it){
-                is Resource.Loading -> setState(BaseState.Loading)
-                is Resource.Success -> _breakingNews.value = it.data!!
-                is Resource.Error -> setState(BaseState.Error((it.message as UiText.DynamicString).value)) //need to change datatype of error message
-            }
-        }
-    }
 }
