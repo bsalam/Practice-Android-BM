d93c650284f684538aa683e9bb345882787a1059
Mohamed <mohamed0909samy@gmail.com>
2023-03-11 22:41:28 +0200

    Added NewsRepository

---
+++ app/src/main/java/com/example/composenewsapp/domain/repository/NewsRepository.kt
@@ -0,0 +1,14 @@
+package com.example.composenewsapp.domain.repository
+
+import com.example.composenewsapp.domain.model.Article
+import com.example.composenewsapp.domain.model.BreakingNewsQuery
+import com.example.composenewsapp.domain.model.NewsQuery
+import com.example.composenewsapp.utils.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+
+    suspend fun getNews(newsQuery: NewsQuery) : Flow<Resource<List<Article>>>
+
+    suspend fun getBreakingNews(breakingNewsQuery: BreakingNewsQuery) : Flow<Resource<List<Article>>>
+}
