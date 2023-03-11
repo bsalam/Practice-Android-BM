adcc408b18faa226f37b2dbf2984abde6338ebc4
Mohamed <mohamed0909samy@gmail.com>
2023-03-11 22:27:44 +0200

    Update files

--- app/src/main/java/com/example/composenewsapp/presentation/news/NewsViewModel.kt
+++ app/src/main/java/com/example/composenewsapp/presentation/news/NewsViewModel.kt
@@ -1,27 +1,55 @@
 package com.example.composenewsapp.presentation.news
 
 import androidx.lifecycle.viewModelScope
+import com.example.composenewsapp.domain.model.Article
+import com.example.composenewsapp.domain.model.BreakingNewsQuery
+import com.example.composenewsapp.domain.model.NewsQuery
+import com.example.composenewsapp.domain.use_cases.BreakingNewsUseCase
+import com.example.composenewsapp.domain.use_cases.NewsUseCase
 import com.example.composenewsapp.presentation.base.BaseState
 import com.example.composenewsapp.presentation.base.BaseViewModel
+import com.example.composenewsapp.utils.Resource
+import com.example.composenewsapp.utils.UiText
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NewsViewModel @Inject constructor(
-
+    private val newsUseCase: NewsUseCase,
+    private val breakingNewsUseCase: BreakingNewsUseCase
 ) : BaseViewModel(BaseState.Empty) {
-    private val _newsState = MutableStateFlow(NewsState(baseState = BaseState.Empty))
+
+    private val _newsState = MutableStateFlow(NewsState(state = BaseState.Empty))
     val newsState: StateFlow<NewsState> get() = _newsState
 
-    fun requestNews() {
-        setState(BaseState.Loading)
-        viewModelScope.launch(Dispatchers.IO) {
-            // safeApiCall { }
-        }
+    private val _news = MutableStateFlow(listOf<Article>())
+    val news: StateFlow<List<Article>> get() = _news
+
+    private val _breakingNews = MutableStateFlow(listOf<Article>())
+    val breakingNews: StateFlow<List<Article>> get() = _news
+
+    fun requestNews(newsQuery: NewsQuery) = viewModelScope.launch(Dispatchers.IO) {
+         newsUseCase(newsQuery).collect {
+             when(it){
+                 is Resource.Loading -> setState(BaseState.Loading)
+                 is Resource.Success -> _news.value = it.data!!
+                 is Resource.Error -> setState(BaseState.Error((it.message as UiText.DynamicString).value)) //need to change datatype of error message
+             }
+         }
     }
 
+    fun requestBreakingNews(breakingNewsQuery: BreakingNewsQuery) = viewModelScope.launch(Dispatchers.IO) {
+        breakingNewsUseCase(breakingNewsQuery).collect {
+            when(it){
+                is Resource.Loading -> setState(BaseState.Loading)
+                is Resource.Success -> _breakingNews.value = it.data!!
+                is Resource.Error -> setState(BaseState.Error((it.message as UiText.DynamicString).value)) //need to change datatype of error message
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/composenewsapp/utils/Resource.kt
+++ app/src/main/java/com/example/composenewsapp/utils/Resource.kt
@@ -3,4 +3,5 @@ package com.example.composenewsapp.utils
 sealed class Resource<T>(val data: T? = null, val message: UiText? = null) {
     class Success<T>(data: T?) : Resource<T>(data)
     class Error<T>(message: UiText?, data: T? = null) : Resource<T>(data, message)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
 }
