2de234267609d1e261f5ad753aa531d53c11cfab
Seif Mohamed <seifeldinmohamed101@gmail.com>
2023-03-19 16:18:44 +0200

    - Refactor MainActivity.kt - edit NewsViewModel.kt

--- app/src/main/java/com/example/composenewsapp/MainActivity.kt
+++ app/src/main/java/com/example/composenewsapp/MainActivity.kt
@@ -1,47 +1,60 @@
 package com.example.composenewsapp
 
 import android.annotation.SuppressLint
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.composenewsapp.domain.model.NewsQuery
 import com.example.composenewsapp.presentation.base.BaseActivity
 import com.example.composenewsapp.presentation.news.NewsViewModel
+import com.example.composenewsapp.presentation.theme.ComposeNewsAppTheme
 import com.example.composenewsapp.screen.ArticleList
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
-    @Composable
-    override fun Content() {
-        NewsScreen()
-    }
-
-}
-
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@Composable
-fun NewsScreen(
-    viewModel: NewsViewModel = hiltViewModel()
-) {
-    val query = NewsQuery(
-        searchStatement = "Technology",
-    )
-    viewModel.requestNews(query)
-    val articles = viewModel.news.collectAsState().value
-
-    Scaffold(
-        topBar = {
-            TopAppBar() {
-                Text("News App BM")
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeNewsAppTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    NewsScreen()
+                }
             }
         }
-    ) {
-        ArticleList(
-            articles = articles,
-        )
+    }
+
+    @Composable
+    fun NewsScreen(
+        viewModel: NewsViewModel = hiltViewModel()) {
+        LaunchedEffect(key1 = Unit) {
+            val query = NewsQuery(
+                searchStatement = "Technology",
+            )
+            viewModel.requestNews(query)
+        }
+
+        val newsState = viewModel.newsState.collectAsState().value
+        if (newsState.news.isEmpty()) {
+            HandleUI(state = newsState.baseState)
+        } else {
+            Scaffold(
+                topBar = {
+                    TopAppBar() {
+                        Text("News App BM")
+                    }
+                }
+            ) {
+                ArticleList(
+                    articles = newsState.news,
+                    paddingValues = it
+                )
+            }
+        }
     }
 }
-
--- app/src/main/java/com/example/composenewsapp/presentation/news/NewsViewModel.kt
+++ app/src/main/java/com/example/composenewsapp/presentation/news/NewsViewModel.kt
@@ -2,7 +2,6 @@ package com.example.composenewsapp.presentation.news
 
 import android.util.Log
 import androidx.lifecycle.viewModelScope
-import com.example.composenewsapp.domain.model.Article
 import com.example.composenewsapp.domain.model.NewsQuery
 import com.example.composenewsapp.domain.use_cases.FetchNewsUseCase
 import com.example.composenewsapp.presentation.base.BaseState
@@ -10,8 +9,7 @@ import com.example.composenewsapp.presentation.base.BaseViewModel
 import com.example.composenewsapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,21 +18,21 @@ class NewsViewModel @Inject constructor(
     private val fetchNewsUseCase: FetchNewsUseCase,
 ) : BaseViewModel() {
 
-    private val _news = MutableStateFlow(listOf<Article>())
-    val news: StateFlow<List<Article>> get() = _news
+    private val _newsState = MutableStateFlow<NewsState>(NewsState())
+    val newsState: StateFlow<NewsState> get() = _newsState
 
     fun requestNews(newsQuery: NewsQuery) {
         Log.d("---", "request news $newsQuery")
-        setState(BaseState.Loading)
+        _newsState.value = _newsState.value.copy(baseState = BaseState.Loading)
         viewModelScope.launch(Dispatchers.IO) {
             executeUseCase { fetchNewsUseCase(newsQuery) }.let {
-                when(it){
+                when (it) {
                     is Resource.Success -> {
-                        Log.d("---", "Success ${it.data}")
-                        _news.value = it.data
+                        _newsState.value = _newsState.value.copy(news = it.data)
                     }
                     is Resource.Error -> {
-                        setState(BaseState.Error(it.message))
+                        isFirstTime = true
+                        _newsState.value = _newsState.value.copy(baseState = BaseState.Error(it.message))
                     }
                 }
             }
