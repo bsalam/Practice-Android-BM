a9150deb602a0a3b0d70c0228e7bf76d19b9ee56
Seif Mohamed <seifeldinmohamed101@gmail.com>
2023-03-22 12:39:08 +0200

    - try to handle the thrown exception

--- app/src/main/java/com/example/composenewsapp/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/example/composenewsapp/presentation/base/BaseViewModel.kt
@@ -1,8 +1,7 @@
 package com.example.composenewsapp.presentation.base
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.example.composenewsapp.utils.Resource
-import com.example.composenewsapp.utils.UiText
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
@@ -13,28 +12,51 @@ import javax.inject.Inject
 @HiltViewModel
 open class BaseViewModel @Inject constructor() : ViewModel() {
 
-    private val _baseState = MutableStateFlow<BaseState>(BaseState.Empty)
+    private val _baseState = MutableStateFlow<BaseState>(BaseState.Loading)
     val baseState: StateFlow<BaseState> get() = _baseState
     var isFirstTime: Boolean = true
 
     private val exceptionHandler = CoroutineExceptionHandler { _, exception ->
-        // _baseState.value = _baseState.value.copy(baseState = BaseState.Error(UiText.DynamicString(exception.message.toString())))
-        _baseState.value = BaseState.Error(errorMessage = UiText.DynamicString(exception.message.toString()))
+        Log.d("---", "in exception handler ${exception.message.toString()}")
+        isFirstTime = true
+        _baseState.value = BaseState.Error(errorMessage = exception.message.toString())
     }
 
     fun setLoadingState() {
-       //  _baseState.value = _baseState.value.copy(baseState = BaseState.Loading)
+        //  _baseState.value = _baseState.value.copy(baseState = BaseState.Loading)
         _baseState.value = BaseState.Loading
     }
 
-    fun setErrorState(errorMessage: UiText) {
+    fun setErrorState(errorMessage: String) {
         // _baseState.value = _baseState.value.copy(baseState = BaseState.Error(errorMessage))
         _baseState.value = BaseState.Error(errorMessage)
     }
 
-    suspend fun <T : Any> executeUseCase(call: suspend () -> Resource<T>): Resource<T> {
-        return withContext(Dispatchers.IO + exceptionHandler) {
-            call.invoke()
+    suspend fun <T : Any> executeUseCase(call: suspend () -> T): T {
+//         withContext(Dispatchers.IO + exceptionHandler) {
+//            call.invoke().let {
+//                Log.d("---", "return form excecut: ${it.toString()}")
+//                when(it){
+//                    is Resource.Error -> throw it.throwable
+//                    is Resource.Success -> return@let it.data
+//                }
+//            }
+//        }
+        return withContext(Dispatchers.IO + CoroutineExceptionHandler { _, throwable ->
+            // this code didn't execute
+            Log.d("---", "exception: ${throwable.toString()}")
+            isFirstTime = true
+            _baseState.value = BaseState.Error(errorMessage = throwable.message.toString())
+        }) {
+            Log.d("---", "call invoke")
+            try {
+                call.invoke()
+            }
+            catch (e:Exception){
+                isFirstTime = true
+                _baseState.value = BaseState.Error(errorMessage = e.message.toString())
+                throw e
+            }
         }
     }
 }
