3c325eb61c8cec636d08f2421ba8ef9ac01cd25b
Seif Mohamed <seifeldinmohamed101@gmail.com>
2023-03-22 12:40:42 +0200

    - update NewsViewModel.kt

--- app/src/main/java/com/example/composenewsapp/presentation/news/NewsViewModel.kt
+++ app/src/main/java/com/example/composenewsapp/presentation/news/NewsViewModel.kt
@@ -2,12 +2,12 @@ package com.example.composenewsapp.presentation.news
 
 import android.util.Log
 import androidx.lifecycle.viewModelScope
+import com.example.composenewsapp.domain.model.Article
 import com.example.composenewsapp.domain.model.NewsQuery
 import com.example.composenewsapp.domain.use_cases.FetchNewsUseCase
-import com.example.composenewsapp.presentation.base.BaseState
 import com.example.composenewsapp.presentation.base.BaseViewModel
-import com.example.composenewsapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -17,24 +17,22 @@ import javax.inject.Inject
 class NewsViewModel @Inject constructor(
     private val fetchNewsUseCase: FetchNewsUseCase,
 ) : BaseViewModel() {
+    private val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+        Log.d("---", "in newsViewModel exception handler ${exception.message.toString()}")
+    }
+    private val _newsState = MutableStateFlow<List<Article>>(emptyList())
+    val newsState: StateFlow<List<Article>> get() = _newsState
 
-    private val _newsState = MutableStateFlow<NewsState>(NewsState())
-    val newsState: StateFlow<NewsState> get() = _newsState
+//    private val _errorMessages = MutableSharedFlow<String>()
+//    val errorMessages: SharedFlow<String> = _errorMessages
 
     fun requestNews(newsQuery: NewsQuery) {
-        Log.d("---", "request news $newsQuery")
-        _newsState.value = _newsState.value.copy(baseState = BaseState.Loading)
-        viewModelScope.launch(Dispatchers.IO) {
+        //Log.d("---", "request news $newsQuery")
+       //  _newsState.value = _newsState.value.copy(baseState = BaseState.Loading)
+        setLoadingState()
+        viewModelScope.launch(Dispatchers.IO+exceptionHandler) {
             executeUseCase { fetchNewsUseCase(newsQuery) }.let {
-                when (it) {
-                    is Resource.Success -> {
-                        _newsState.value = _newsState.value.copy(news = it.data)
-                    }
-                    is Resource.Error -> {
-                        isFirstTime = true
-                        _newsState.value = _newsState.value.copy(baseState = BaseState.Error(it.message))
-                    }
-                }
+                _newsState.value = it
             }
         }
     }
