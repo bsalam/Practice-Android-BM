35e563a89653f041f194baa2fa0933d88538e9fe
Seif Mohamed <seifeldinmohamed101@gmail.com>
2023-03-22 12:36:58 +0200

    - Refactor BaseActivity.kt by throwing the exception

--- app/src/main/java/com/example/composenewsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/composenewsapp/domain/repository/NewsRepository.kt
@@ -8,6 +8,6 @@ import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
 
-    suspend fun getNews(newsQuery: NewsQuery) : Resource<List<Article>>
+    suspend fun getNews(newsQuery: NewsQuery) :List<Article>
     suspend fun getBreakingNews(breakingNewsQuery: BreakingNewsQuery) : Flow<Resource<List<Article>>>
 }
