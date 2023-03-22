3ff087c90c28c4f406bb3799147b73457033fcb6
Seif Mohamed <seifeldinmohamed101@gmail.com>
2023-03-22 12:37:54 +0200

    - make error message of types string not uiText for simplicity for now

--- app/src/main/java/com/example/composenewsapp/presentation/base/BaseState.kt
+++ app/src/main/java/com/example/composenewsapp/presentation/base/BaseState.kt
@@ -5,7 +5,8 @@ import com.example.composenewsapp.utils.UiText
 sealed class BaseState {
     object Empty : BaseState()
     object Loading : BaseState()
-    data class Error(val errorMessage: UiText) : BaseState()
+    data class Error(val errorMessage: String) : BaseState() // replace this errorMessage with sealed class that will be made for errorTypes
+
     data class NoInternetConnection(val message: UiText) : BaseState()
 }
 
