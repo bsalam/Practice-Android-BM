cf8fb5f0b738a48aeedac4353649768c7c9d7a27
Mohamed <mohamed0909samy@gmail.com>
2023-03-25 19:41:02 +0200

    cleaning

--- app/src/main/java/com/example/composenewsapp/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/example/composenewsapp/presentation/base/BaseViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.composenewsapp.presentation.base
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.composenewsapp.domain.exception_handler.CustomException
@@ -16,16 +15,16 @@ import javax.inject.Inject
 @HiltViewModel
 open class BaseViewModel @Inject constructor() : ViewModel() {
 
-    private val _myState = MutableStateFlow<BaseState>(BaseState.Empty)
-    val state: StateFlow<BaseState> get() =  _myState.asStateFlow()
+    private val _state = MutableStateFlow<BaseState>(BaseState.Empty)
+    val state: StateFlow<BaseState> get() =  _state.asStateFlow()
     var isFirstTime: Boolean = true
     private val exceptionHandler = CoroutineExceptionHandler { _, exception ->
-        _myState.value = BaseState.Error((exception as CustomException).toError())
+        _state.value = BaseState.Error((exception as CustomException).toError())
     }
 
     fun setState(newState: BaseState) {
         // _myState.value = _myState.value.copy(baseState = newState)
-        _myState.value = newState
+        _state.value = newState
     }
 
     suspend fun <T : Any> executeUseCase(call: suspend () -> T) {
