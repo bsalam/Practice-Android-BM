6f56de72fba72a0189d7d11b0659d03cabec18c3
Mohamed <mohamed0909samy@gmail.com>
2023-04-18 17:12:52 +0200

    Remove withTimeout function usage, and use query mappers.

--- data/src/main/java/com/example/data/repository/NewsRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/NewsRepositoryImpl.kt
@@ -3,21 +3,20 @@ package com.example.data.repository
 import android.net.ConnectivityManager
 import com.example.data.data_source.remote.api.NewsApi
 import com.example.data.mapper.toArticleDomainModel
+import com.example.data.mapper.toBreakingNewsQueryDataModel
 import com.example.data.mapper.toCustomExceptionDomainModel
-import com.example.data.utils.Constants.Companion.SEARCH_NEWS_TIMEOUT
+import com.example.data.mapper.toNewsQueryDataModel
 import com.example.data.utils.isConnectedToInternet
 import com.example.domain.models.CustomExceptionDomainModel
 import com.example.domain.models.ArticleDomainModel
 import com.example.domain.models.BreakingNewsQueryDomainModel
 import com.example.domain.models.NewsQueryDomainModel
 import com.example.domain.repository.NewsRepository
-import kotlinx.coroutines.withTimeout
 import javax.inject.Inject
 
 class NewsRepositoryImpl @Inject constructor(
     private val api: NewsApi,
     private val connectivityManager: ConnectivityManager,
-   // private val exceptionHandler: ExceptionHandler
 ) : NewsRepository {
 
     override suspend fun getNews(
@@ -26,17 +25,18 @@ class NewsRepositoryImpl @Inject constructor(
         if (!connectivityManager.isConnectedToInternet())
             throw CustomExceptionDomainModel.NoInternetConnectionExceptionDomainModel
         return try {
-            withTimeout(SEARCH_NEWS_TIMEOUT){
-                val response = api.getNews(
-                    searchStatement = newsQueryDomainModel.searchStatement,
-                    pageNumber = newsQueryDomainModel.pageNumber,
-                    fromDate = newsQueryDomainModel.fromDate,
-                    toDate = newsQueryDomainModel.toDate,
-                    language = newsQueryDomainModel.language
-                )
+            val query = newsQueryDomainModel.toNewsQueryDataModel()
 
-                response.articles.map { it.toArticleDomainModel() }
-            }
+            val response = api.getNews(
+                searchStatement = query.searchStatement,
+                pageNumber = query.pageNumber,
+                fromDate = query.fromDate,
+                toDate = query.toDate,
+                language = query.language
+            )
+
+            response.articles.map { it.toArticleDomainModel() }
+
         } catch (e: Exception) {
             throw e.toCustomExceptionDomainModel()
         }
@@ -49,15 +49,16 @@ class NewsRepositoryImpl @Inject constructor(
             throw CustomExceptionDomainModel.NoInternetConnectionExceptionDomainModel
 
         return try {
-            withTimeout(SEARCH_NEWS_TIMEOUT){
-                val response = api.getBreakingNews(
-                    searchStatement = breakingNewsQueryDomainModel.searchStatement,
-                    pageNumber = breakingNewsQueryDomainModel.pageNumber,
-                    countryCode = breakingNewsQueryDomainModel.countryCode
-                )
+            val query = breakingNewsQueryDomainModel.toBreakingNewsQueryDataModel()
 
-                response.articles.map { it.toArticleDomainModel() }
-            }
+            val response = api.getBreakingNews(
+                searchStatement = query.searchStatement,
+                pageNumber = query.pageNumber,
+                countryCode = query.countryCode
+            )
+
+            response.articles.map { it.toArticleDomainModel() }
+
         } catch (e: Exception) {
             throw e.toCustomExceptionDomainModel()
         }
