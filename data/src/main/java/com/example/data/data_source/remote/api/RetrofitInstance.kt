0faffcff667942215ab3e9aae15d25371d708fac
Mohamed <mohamed0909samy@gmail.com>
2023-04-18 17:08:28 +0200

    Setting call timeout and read timeout for retrofit calls.

--- data/src/main/java/com/example/data/data_source/remote/api/RetrofitInstance.kt
+++ data/src/main/java/com/example/data/data_source/remote/api/RetrofitInstance.kt
@@ -1,17 +1,25 @@
 package com.example.data.data_source.remote.api
 
 import com.example.data.utils.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
 
     companion object {
 
         private val retrofit by lazy {
+            val client = OkHttpClient.Builder()
+                .callTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS)
+                .build()
+
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create())
+                .client(client)
                 .build()
         }
 
