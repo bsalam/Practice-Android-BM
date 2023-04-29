d8f7fefe39adf40f2ac4b24111507e4fb57da295
Seif Mohamed <seifeldinmohamed101@gmail.com>
2023-04-29 07:32:31 +0200

    - Edit NewsViewModel.kt

--- presentation/src/main/java/com/example/presentation/screen/news/NewsViewModel.kt
+++ presentation/src/main/java/com/example/presentation/screen/news/NewsViewModel.kt
@@ -5,23 +5,25 @@ import com.example.domain.models.NewsQueryDomainModel
 import com.example.domain.use_cases.FetchNewsUseCase
 import com.example.presentation.base.BaseState
 import com.example.presentation.base.BaseViewModel
+import com.example.presentation.utils.DispatcherProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class NewsViewModel @Inject constructor(
     private val fetchNewsUseCase: FetchNewsUseCase,
-) : BaseViewModel() {
-
+    dispatcher: DispatcherProvider
+) : BaseViewModel(dispatcher) {
     private val _newsState = MutableStateFlow<List<ArticleDomainModel>?>(null)
-    val newsState: StateFlow<List<ArticleDomainModel>?> get() = _newsState
-
+    val newsState get() = _newsState.asStateFlow()
     fun requestNews(newsQueryDomainModel: NewsQueryDomainModel) {
         setState(BaseState.Loading)
         executeUseCase {
             _newsState.value = fetchNewsUseCase(newsQueryDomainModel)
+            delay(100L) // for testing purposes
             setState(BaseState.Empty)
         }
     }
